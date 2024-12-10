4205bbf4e7133f6f7cd465981b2db72bf3b8125e
deplanty <yvon.deplante@gmail.com>
2024-12-10 10:44:36 +0100

    Day 7 done

--- day7.py
+++ day7.py
@@ -39,3 +39,32 @@ for result, values in calibrations.items():
             break
 
 print("Part 1:", total)
+
+## Part 2
+
+
+def calc2(val1: int, op: str, val2: int):
+    if op == "+":
+        return val1 + val2
+    elif op == "*":
+        return val1 * val2
+    elif op == "|":
+        return int(str(val1) + str(val2))
+
+
+total = 0
+for result, values in calibrations.items():
+    operations = itertools.product("+*|", repeat=len(values) - 1)
+    for sequence in operations:
+        values_tmp = copy.deepcopy(values)
+        for op in sequence:
+            val1 = values_tmp.pop(0)
+            val2 = values_tmp.pop(0)
+            test = calc2(val1, op, val2)
+            values_tmp.insert(0, test)
+
+        if test == result:
+            total += result
+            break
+
+print("Part 2:", total)
