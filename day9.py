15b70c6ce255f00f4ca7a3691f03cb1c56139a2f
deplanty <yvon.deplante@gmail.com>
2024-12-12 17:21:04 +0100

    WIP: Day 9 part 2

--- day9.py
+++ day9.py
@@ -1,7 +1,7 @@
 from src.reader import Reader
 
 
-def calc_checksum(line: str) -> int:
+def calc_checksum(line: list[int]) -> int:
     total = 0
     for i, val in enumerate(line):
         if val is None:
@@ -11,7 +11,7 @@ def calc_checksum(line: str) -> int:
     return total
 
 
-example = False
+example = True
 
 ## Part 1
 
@@ -43,5 +43,86 @@ while i < j:
 total = calc_checksum(storage)
 print("Part 1:", total)
 
-
 ## Part 2
+
+
+class MemorySlot:
+    def __init__(self, size: int, iid: int = None):
+        self.size = size
+        self.iid = iid
+
+    def __str__(self):
+        return f"{self.iid}x{self.size}"
+
+    def __repr__(self):
+        return str(self)
+
+    def checksum(self) -> int:
+        if self.iid is None:
+            return 0
+
+        cs = 0
+        for i in range(self.size):
+            cs += i * self.iid
+        return cs
+
+    def is_file(self) -> bool:
+        return self.iid is not None
+
+    def is_empty(self) -> bool:
+        return self.iid is None
+
+    def can_fit(self, slot: "MemorySlot") -> bool:
+        return self.size >= slot.size
+
+    def clear(self):
+        self.iid = None
+
+
+def calc_checksum(line: list[MemorySlot]) -> int:
+    total = 0
+    i = 0
+    for slot in line:
+        if slot.is_empty():
+            break
+        for _ in range(slot.size):
+            total += i * val
+            i += 1
+
+    return total
+
+
+storage = list()
+for i, size in enumerate(memory):
+    if i % 2 == 0:
+        iid = i // 2
+    else:
+        iid = None
+    storage.append(MemorySlot(size, iid))
+
+
+for j in range(len(storage), -1, -1):
+    for i in range(j):
+        if storage[i].is_file():
+            continue
+
+        if storage[i].can_fit(storage[j]):
+            size_i = storage[i].size
+            size_j = storage[j].size
+            storage[i] = storage.pop(j)
+            storage.insert(i + 1, MemorySlot(size_i - size_j, None))
+
+
+line = list()
+for slot in storage:
+    if slot.is_empty():
+        iid = "."
+    else:
+        iid = str(slot.iid)
+    for _ in range(slot.size):
+        line.append(iid)
+
+print("".join(line))
+
+total = calc_checksum(storage)
+print("Part 2:", total)
