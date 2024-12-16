eec5be8e75e1cfe1762809d746c1985335112c94
deplanty <yvon.deplante@gmail.com>
2024-12-16 21:11:24 +0100

    Day 14 done

--- day14.py
+++ day14.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from src import Debug, Reader, Index
 
 
@@ -27,6 +30,9 @@ class Robot:
 
 debug = Debug()
 example = False
+debug.disable()
+
+## Part 1
 
 robots: list[Robot] = list()
 with Reader(14, example) as reader:
@@ -74,3 +80,52 @@ total = 1
 for count in quadrant.values():
     total *= count
 print("Part 1:", total)
+
+## Part 2
+
+robots: list[Robot] = list()
+with Reader(14, example) as reader:
+    for position, velocity in reader.iter_split():
+        px, py = position[2:].split(",")
+        vx, vy = velocity[2:].split(",")
+
+        robots.append(Robot(Index(py, px), Index(vy, vx)))
+
+size: Index = {
+    True: Index(7, 11),  # Example size
+    False: Index(103, 101),  # Input size
+}.get(example)
+
+image = np.zeros(size.ij)
+entropys = list()
+answers = list()
+count = 10_000
+i = 1
+while count > 0:
+    image = np.zeros(size.ij)
+    for robot in robots:
+        robot.move()
+        robot.fit(size)
+
+        image[robot.position.ij] += 1
+
+    marg = np.histogramdd(np.ravel(image), bins=256)[0] / image.size
+    marg = list(filter(lambda p: p > 0, np.ravel(marg)))
+    entropy = float(-np.sum(np.multiply(marg, np.log2(marg))))
+    entropys.append(entropy)
+
+    if entropy < 0.2785:
+        answers.append(entropy)
+        plt.figure()
+        plt.imshow(image, cmap="grey")
+        plt.title(f"Step n°{i}, entropy={entropy}")
+
+    count -= 1
+    i += 1
+
+
+plt.figure()
+plt.scatter(range(len(entropys)), entropys, c="red", edgecolors="red", alpha=0.5)
+plt.show()
+
+print("Part 2:", answers)
